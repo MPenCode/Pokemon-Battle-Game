b25697e51b6800f6ed70bfd78158decc71a055c9
Lukitas <lvd.follert@gmail.com>
2024-11-08 13:22:06 +0100

    submit score, create user

--- Backend/models/usersModel.js
+++ Backend/models/usersModel.js
@@ -1,41 +1,44 @@
 import { Schema, model } from 'mongoose';
 
-const userSchema = new Schema({
+const userSchema = new Schema(
+  {
     username: {
-        type: String,
-        required: true,
-        unique: true,
+      type: String,
+      required: true,
+      unique: true,
     },
     password: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     exp: {
-        type: Number,
-        required: true,
+      type: Number,
+      required: true,
+      default: 0,
     },
     matchesPlayed: {
-        type: Number,
-        required: true,
+      type: Number,
+      required: true,
+      default: 0,
     },
+  },
+  {
+    timestamps: true,
+    toJSON: {
+      transform: (doc, ret) => {
+        delete ret.password;
+        return ret;
+      },
     },
-    {
-        timestamps: true,
-        toJSON: {
-            transform: (doc, ret) => {
-                delete ret.password; // Exclude password
-                return ret;
-            },
-        },
-        toObject: {
-            virtuals: true,
-            versionKey: false,
-            transform: (doc, ret) => {
-                delete ret.password; // Exclude password
-                return ret;
-            },
-        },
+    toObject: {
+      virtuals: true,
+      versionKey: false,
+      transform: (doc, ret) => {
+        delete ret.password;
+        return ret;
+      },
     },
+  }
 );
 
 export default model('User', userSchema);
--- Backend/server.js
+++ Backend/server.js
@@ -1,33 +1,32 @@
 import express, { json } from 'express';
 import { config } from 'dotenv';
 import cors from 'cors';
-import './db/mongoDB.js';
 import cookieParser from 'cookie-parser';
-
+import connectToDatabase from './db/mongoDB.js';
 import { errorHandler } from './utils/errorHandler.js';
 import usersRouter from './routes/usersRoutes.js';
 
 config();
+
 const PORT = process.env.PORT || 5000;
-
 const app = express();
+
+app.use(json());
 app.use(
-  json(),
-  cors({ origin: process.env.CLIENT_URL, credentials: true }),
-  cookieParser()
+  cors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+  })
 );
+app.use(cookieParser());
 
-app.get('/', (req, res) => {
-  res.json({ message: 'Server is running!' });
-});
+app.use('/api/v1/users', usersRouter);
 
-app.use(`/api/v1/users`, usersRouter);
-
-app.get('*', (req, res) => {
-  res.status(404).json({ message: 'page not found!' });
-});
+app.get('/', (req, res) => res.json({ message: 'Server is running!' }));
+app.get('*', (req, res) => res.status(404).json({ message: 'Page not found!' }));
 
 app.use(errorHandler);
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+
+connectToDatabase().then(() =>
+  app.listen(PORT, () => console.log(`Server is running on http://localhost:${PORT}`))
+);
